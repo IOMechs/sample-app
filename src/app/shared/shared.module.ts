88e9683bc631b7baf66fc3407d946deacc26f762
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2019-09-27 01:33:39 +0500

    feat(lazy-loading): Feature lazy loading added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AlertComponent } from './components/alert/alert.component';
 import { AlertService } from './services/alert.service';
 import { ScrollableDirective } from './directives/scrollable.directive';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LazyLoadImageModule
   ],
   declarations: [
     AlertComponent,
-    ScrollableDirective
+    ScrollableDirective,
+    LazyImageComponent
   ],
   providers: [
     AlertService
@@ -20,7 +24,8 @@ import { ScrollableDirective } from './directives/scrollable.directive';
   entryComponents: [],
   exports: [
     AlertComponent,
-    ScrollableDirective
+    ScrollableDirective,
+    LazyImageComponent
   ]
 })
 export class SharedModule { }
